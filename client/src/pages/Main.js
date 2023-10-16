18bf40cc1f713c5e27c1dc47c083c01133c4194f
MireilleNyoubissi <nmireillemat@yahoo.fr>
2023-10-16 04:48:11 +0100

    added loginmain and navigation files

---
+++ client/src/pages/Main.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ListedQtnThread from "../components/ListedQtnThread/ListedQtnThread";
+import "./Home.css";
+
+const Main = () => {
+	return (
+		<main className="main" role="main">
+			<div className="main-title-box text-center">
+				<h1 className="text-wrap">TheNerds Q & A APP</h1>
+				<p className="main-title-p text-wrap fs-6">
+					Ask a code-related question
+				</p>
+			</div>
+			<div className="questionsDiv">
+				<ListedQtnThread />
+			</div>
+		</main>
+	);
+};
+export default Main;
